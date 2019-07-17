6b0a231f841700aa075491fcdbc6987ce6199a8c
zenoven <zenoven@gmail.com>
2019-07-17 16:45:07 +0800

    feat: replace with cupertino style

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,22 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:zivi/const.dart';
 import 'package:zivi/routes.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  return runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return CupertinoApp(
+      localizationsDelegates: const <LocalizationsDelegate<dynamic>>[
+        DefaultMaterialLocalizations.delegate,
+        DefaultWidgetsLocalizations.delegate,
+      ],
       title: appConfig['name'],
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
       home: routes['summary'](context),
       routes: routes,
     );
